133a8f5e5ee61a718b9809faae18b78ab70d6502
guitarkeegan <keegananglim@gmail.com>
2023-03-07 11:48:56 -0800

    add cost field to sheet music and migrate changes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -29,6 +29,7 @@ async function main() {
           "Tired waltz is mostly writen in 3/4 as you might expect, but the A and C sections also contain a few measure of 4/4. The odd phrasing follows the melody however, and shouldn't be too difficult to the the hang of. It has a 'blowing bridge' that should be friendly for beginning improvisors, due to the static quality of the harmony at the section.",
         downloadUrl:
           "https://docs.google.com/document/d/1bZECyodym6tl2AzCcr92i1I6hpCctqIL/edit?usp=sharing&ouid=113182304659835052546&rtpof=true&sd=true",
+        cost: 39.99
       },
       {
         title: "Something You Wouldn't Say Out Loud",
@@ -37,6 +38,7 @@ async function main() {
           "Something You Wouldn't Say Out Loud is an advanced arrangment that incorperates angular melodies, disonnant harmonies, and odd time signatures. While the melodies can be challenging at first, they are fun to play once they have been worked on by the individual members. The chord progression at the improvisation section is contains interesting non-functional harmonies, along with a quirky texture between the bass and ostinato part. Challenging, but a lot of fun!",
         downloadUrl:
           "https://docs.google.com/document/d/1bZECyodym6tl2AzCcr92i1I6hpCctqIL/edit?usp=sharing&ouid=113182304659835052546&rtpof=true&sd=true",
+        cost: 39.99,
       },
     ],
   });
