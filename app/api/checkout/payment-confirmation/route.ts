aa69187a44061919bd789cacd7d64f466ff8bb58
guitarkeegan <keegananglim@gmail.com>
2023-03-23 22:58:57 -0700

    still trying to get rawbody

--- app/api/checkout/payment-confirmation/route.ts
+++ app/api/checkout/payment-confirmation/route.ts
@@ -1,22 +1,23 @@
 import { stripe } from "@/lib/stripe"
-import { Buffer } from "node:buffer"
-// import Cors from "micro-cors"
+import getRawBody from 'raw-body';
+import { NextApiRequest } from "next";
+import { IncomingMessage } from "http";
 
-export async function POST(req: Request){
-
-    const payload = await req.json()
-    const buf = Buffer.from(payload)
+export async function POST(req: NextApiRequest){
     
-    const sig = req.headers.get('stripe-signature') as string
+    console.log("getting raw body...")
+    const body = getRawBody(req)
+    console.log("getting headers")
+    const sig = req.headers["stripe-signature"] as string | string[]
     console.log("sig: ", sig)
 
     let event;
-
     const endpointSecret = process.env.WEBHOOK_SECRET_LOCAL as string
+    console.log("got endpoint secret", endpointSecret)
 
     try {
         console.log("constructing event...")
-        event = stripe.webhooks.constructEvent(buf.toString(), sig, endpointSecret)
+        event = stripe.webhooks.constructEvent(body, sig, endpointSecret)
     } catch (error) {
         console.error(error)
         return new Response(`Webhook error: ${error}`, {
@@ -28,3 +29,9 @@ export async function POST(req: Request){
         status: 200,
     })
 }
+
+export const config = {
+    api: {
+      bodyParser: false,
+    },
+  };
